4d6c78fc2a737cdee7b593dd863e26d918814c32
maltegaming595-cyber <maltegaming595@gmail.com>
2026-02-01 10:44:39 +0100

    Delete src directory

--- src/discord_download.js
+++
@@ -1,7 +0,0 @@
-const DownloadEvent = require('./models/DownloadEvent');
-
-async function logDiscordDownload(discordId, resource) {
-  await DownloadEvent.create({ discordId, source: 'discord', resource });
-}
-
-module.exports = { logDiscordDownload };
